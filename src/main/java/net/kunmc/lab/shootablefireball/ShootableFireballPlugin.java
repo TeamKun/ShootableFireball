5360cd7f1efaaf86b2929a9c0e464fef59226d18
Maru32768 <maru32768@gmail.com>
2022-11-28 01:49:29 +0900

    init

--- src/main/java/net/kunmc/lab/shootablefireball/ShootableFireballPlugin.java
+++ src/main/java/net/kunmc/lab/shootablefireball/ShootableFireballPlugin.java
@@ -1,8 +1,8 @@
-package net.kunmc.lab.paperplugintemplate;
+package net.kunmc.lab.shootablefireball;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class PaperPluginTemplate extends JavaPlugin {
+public final class ShootableFireballPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
     }
